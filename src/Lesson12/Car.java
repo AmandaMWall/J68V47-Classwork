e7a67dbdb87d0bf947fe7937e9027257dfb3a107
AMANDA WALLIS <30132460@computing.nescol.ac.uk>
2023-12-04 11:29:47 +0000

    add E3

---
+++ src/Lesson12/Car.java
@@ -0,0 +1,12 @@
+package Lesson12;
+
+public class Car {
+    public String make;
+    public String model;
+    public int year;
+    public String vin;
+    public int price;
+    public String colour;
+    public String mileage;
+    public String condition;
+}
--- src/Lesson12/L12E3.java
+++ src/Lesson12/L12E3.java
@@ -1,5 +1,25 @@
 package Lesson12;
+import Lesson12.Car;
 
 public class L12E3 {
+    public static void main(String[] args) {
+        Car ferrari = new Car();
+        ferrari.make = "Ferrari";
+        ferrari.model = "F430";
+        ferrari.year = 2009;
+        ferrari.vin = "3GYVKNEFXAG625569";
+        ferrari.price = 55125;
+        ferrari.colour = "White";
+        ferrari.mileage = "45336";
+        ferrari.condition = "Good";
 
+        System.out.println("--------------------------------------");
+        System.out.format("%s %s (%d)\n", ferrari.make, ferrari.make, ferrari.year);
+        System.out.format("Vin: %s\n", ferrari.vin);
+        System.out.format("Colour: %s\n", ferrari.colour);
+        System.out.format("Mileage: %s\n", ferrari.mileage);
+        System.out.format("Condition: %s\n", ferrari.condition);
+        System.out.format("Price %d\n", ferrari.price);
+        System.out.println("--------------------------------------");
+    }
 }
